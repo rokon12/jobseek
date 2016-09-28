47ce8e73440ab71707fda6c1833366a640ade5e0
Bazlur Rahman Rokon <anm_brr@yahoo.com>
2016-09-28 21:54:04 +0600

    Update build.gradle

---
+++ libs/install.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+mvn install:install-file -Dfile=prettytime-nlp-4.0.1.Final.jar -DgroupId=org.ocpsoft.prettytime -DartifactId=prettytime-nlp -Dversion=4.0.1.Final -Dpackaging=jar
